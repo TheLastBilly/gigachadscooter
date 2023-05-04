ea88ca4436b295097b875502c9342270fe942008
TheLastBilly <jtmonegro@gmail.com>
2023-05-03 20:00:05 -0400

    disabled init for ssh and networking, added config.txt settings on post-build

---
+++ board/raspberrypi0w/config.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cat board/raspberrypi0w/config.txt > $BASE_DIR/images/rpi-firmware/config.txt
---
+++ board/raspberrypi0w/remove_inits.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm $TARGET_DIR/etc/init.d/S*network
+rm $TARGET_DIR/etc/init.d/S*sshd
+true
